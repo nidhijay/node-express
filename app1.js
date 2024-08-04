cfc4044dc8b54acbeaf3efe35a3d10ffe337c569
Nidhi <p.nidhi2830@gmail.com>
2024-08-04 14:55:18 -0500

    simple node example3

--- app1.js
+++ app1.js
@@ -1,17 +1,23 @@
-const rect = require('./rectangle');
+const rect = require("./3.rectangle_with_callbk.js");
 
- function solveRect(l, w) {
-    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
-    if (l <= 0 || w <= 0) {
-    console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, 
-   ${w}`);
+function solveRectancgle(l, w) {
+  console.log(`Solving for Rectangle: ${l}, ${w}`);
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log(`Error: ${err.message}`);
     } else {
-    console.log(`Area of rectangle: ${rect.area(l, w)}`);
-    console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
+      console.log(`Area of the Rectangle with dimensions: ${l}, ${w} is => ${rectangle.area()}`);
+      console.log(`Perimeter of the Rectangle with dimensions: ${l}, ${w} is => ${rectangle.perimeter()}`);
     }
+  });
+  console.log(`After rect module for Dimensions: ${l} ${w}`);
 }
 
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(5, -3);
+solveRectancgle(1, 2);
+solveRectancgle(1, 0);
+solveRectancgle(0, 2);
+solveRectancgle(0, 0);
+solveRectancgle(1, -2);
+solveRectancgle(-1, 2);
+solveRectancgle(-1, -2);
+solveRectancgle(5, 6);
